01bbbc1b3e536c65e1f7b02291438c3365f68f1c
Todd Werth <twerth@infinitered.com>
2011-05-17 07:51:29 -0700

    Added bashbookmarks. https://github.com/twerth/bashmarks

---
+++ bin/bookmarks.sh
@@ -0,0 +1,73 @@
+# Provides bookmarking ability for folders/directories in bash.
+# 
+# To install, put bookmarks.sh somewhere such as ~/bin, then source it 
+# in your .bashrc file (or other bash startup file):
+#   source ~/bin/bookmarks.sh
+#
+# To bookmark a folder, simply go to that folder, then bookmark it like so:
+#   bookmark foo
+#
+# The bookmark will be named "foo"
+#
+# When you want to get back to that folder use:
+#   go foo
+#
+# To see a list of the bookmarks:
+#   bookmarksshow
+# 
+# Tab completion works, to go to the shoobie folder:
+#   go sho[tab]
+
+bookmarks_file=~/.bookmarks
+
+# Create bookmarks_file it if it doesn't exist
+if [[ ! -f $bookmarks_file ]]; then
+  touch $bookmarks_file
+fi
+
+bookmark (){
+  bookmark_name=$1
+
+  if [[ -z $bookmark_name ]]; then
+    echo 'Invalid name, please provide a name for your bookmark. For example:'
+    echo '  bookmark foo'
+  else
+    bookmark="`pwd`|$bookmark_name" # Store the bookmark as folder|name
+
+    if [[ -z `grep "$bookmark" $bookmarks_file` ]]; then
+      echo $bookmark >> $bookmarks_file
+      echo "Bookmark '$bookmark_name' saved"
+    else
+      echo "Bookmark already existed"
+    fi
+  fi
+} 
+
+# Show a list of the bookmarks
+bookmarksshow (){
+  cat ~/.bookmarks | awk '{ printf "%-40s%-40s%s\n",$1,$2,$3}' FS=\|
+}
+
+go(){
+  bookmark_name=$1
+
+  bookmark=`grep "|$bookmark_name$" "$bookmarks_file"`
+
+  if [[ -z $bookmark ]]; then
+    echo 'Invalid name, please provide a valid bookmark name. For example:'
+    echo '  go foo'
+    echo
+    echo 'To bookmark a folder, go to the folder then do this (naming the bookmark 'foo'):'
+    echo '  bookmark foo'
+  else
+    dir=`echo "$bookmark" | cut -d\| -f1`
+    cd "$dir" 
+  fi
+}
+
+_go_complete(){
+  # Get a list of bookmark names, then grep for what was entered to narrow the list
+  cat $bookmarks_file | cut -d\| -f2 | grep "$2.*"
+}
+
+complete -C _go_complete -o default go 
